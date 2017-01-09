d2a15993c88f61007f9038c5989ef3a08decf512
Bruno Arcanjo <bm.arcanjo@gmail.com>
2017-01-09 21:02:18 -0200

    Personalizando o serviço

--- src/br/com/caelum/estoque/modelo/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/modelo/ws/EstoqueWS.java
@@ -2,8 +2,14 @@ package br.com.caelum.estoque.modelo.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 
@@ -12,9 +18,12 @@ public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();
 	
-	public List<Item> getItens() {
+	@WebMethod(operationName = "todosOsItens")
+	@ResponseWrapper(localName = "itens")
+	@RequestWrapper(localName = "listaItens")
+	@WebResult(name = "itens")
+	public List<Item> getItens(@WebParam(name = "filtros") Filtros filtros) {
 		System.out.println("Chamando getItens()");
-		List<Item> lista = dao.todosItens();
-		return lista;
+		return dao.todosItens();
 	}
 }
--- src/br/com/caelum/estoque/modelo/ws/PublicaWebService.java
+++ src/br/com/caelum/estoque/modelo/ws/PublicaWebService.java
@@ -8,6 +8,8 @@ public class PublicaWebService {
 		EstoqueWS service = new EstoqueWS();
 		String url = "http://localhost:8080/estoquews";
 		
+		System.out.println("Rodando serviço " + url + "?wsdl");
+		
 		Endpoint.publish(url, service);
 	}
 
